9c94bfe71665f2caf692d77557d18239430e8461
Redondave <121982786+Redondave@users.noreply.github.com>
2024-08-17 13:27:15 -0300

    Update game.s

--- game.s
+++ game.s
@@ -2,10 +2,11 @@
 STAGE:		.half 3				# nivel (0 = mapa 1, 1 = mapa 2, 2 = victory, 3 = menu)
 CHAR_POS: 	.half 60, 112			# x, y
 OLD_CHAR_POS:	.half 60, 112			# x, y
-MARIO_STATUS:	.half 1, 3, 1			# numero do sprite (0 = parado, 1 = movendo), dire��o (0 = esquerda, 1 = direita, 2 = cima, 3 = baixo), invencivel (0 = poder, 1 = normal)
+MARIO_STATUS:	.half 1, 3, 1			# numero do sprite (0 = parado, 1 = movendo), direï¿½ï¿½o (0 = esquerda, 1 = direita, 2 = cima, 3 = baixo), invencivel (0 = poder, 1 = normal)
 MARIO_HITBOX1:  .half 0,0			# hitbox do mario
 MARIO_HITBOX2:  .half 0,0
 POINTS:		.half 0				# contagem de pontos para a passagem do mapa / condicao de vitoria
+CURRENT:	.half 0				# contador fisico da pontuacao que sera mostrado no bitmap display
 PRESSED:	.half 0				# endereco para verificar o cheat pressionado (0 = nao, 1 = sim)
 SCOREBOARD:	.string "SCORE"	
 RECORDE:	.string "HIGH"	
@@ -32,30 +33,45 @@ MENU_LOOP:	xori s8, s8, 1			# inverte o frame
 		call KEY2
 		la t0, PRESSED
 		lh t1, 0(t0)			# checa se alguma tecla foi apertada para sair do loop do menu
+		bne t1, zero, PRESS_RES	
 		li a7 32
 		beqz a5, TIMEUP
 		li a0, 750
 		j WAIT
+		
 TIMEUP:		li a0, 1750
+
 WAIT:		ecall				# freeza a mensagem na tela (1 s tela do menu e 2s 'press m') 
-		beqz t1, MENU_LOOP
-		li t1, 0
+		j MENU_LOOP
+		
+PRESS_RES:	li t1, 0
 		sh t1, 0(t0)			# reseta o contador
 
 SETUP:		li s10, 0			# contador para a animacao do sprite
-		li s9, 0			# contador de pontos
+		la a0, black
+		li a1, 0
+		li a2, 0
+		li a5, 0
+		render_map(a0, a1, a2, 320, 240, a5)
+		li a5, 1
+		render_map(a0, a1, a2, 320, 240, a5)
+		# deixa a tela preta em ambos frames
+		li a7, 32
+		li a0, 1000			
+		ecall				# pausa por 1 s
 		la t0, STAGE
 		lh t0, 0(t0)			# carrega qual o nivel que o player esta
 		beqz t0, MAPA1
 		li t2, 2
 		beq t0, t2, VICTORY		# caso o jogador passe da segunda fase (STAGE = 1), ele ganha
-		add s9, s9, t1			# soma ao contador de pontos a pontuacao pre-existente
-		#############			# carrega o endereco do mapa2 em a0
+		la a0, mapa_2			# carrega o endereco do mapa2 em a0
+		mv s2, a0
+		la s3, moedas_2			# carrega o endereco do mapa de moedas 2
 		j PRINT_MAP
 		
 MAPA1:		la a0,mapa_1n			# carrega o endereco do mapa1 em a0
 		mv s2, a0
-		la s3, moedas_1n		# coloca o endere�o do mapa de moedas 
+		la s3, moedas_1n		# coloca o endereço do mapa de moedas 
 		
 PRINT_MAP:	li a1,0				# x = 0
 		li a2,0				# y = 0
@@ -69,6 +85,14 @@ PRINT_MAP:	li a1,0				# x = 0
 		li a5, 0
 		render_map(a0, a1, a2, 320, 240, a5) 
 		# desenha as moedas no mapa
+		la a0, CHAR_POS
+		li t0, 60
+		li t1, 112
+		sh t0, 0(a0)
+		sh t1, 2(a0) 			
+		la a0, OLD_CHAR_POS
+		sh t0, 0(a0)			
+		sh t1, 2(a0)			# reseta a posicao do mario para a inicial (x = 60, y = 112) , assim como OLD_CHAR_POS
 		la a0, mario_down
 		li a1, 60
 		li a2, 112
@@ -107,29 +131,31 @@ LIFE_CONFIG:	la a0, LIFECOUNT		# checa a quantidade de vidas do player para impr
 		beq t0, t1, TWO_L
 THREE_L:	la a0, life_3
 		render(a0, a1, a2, 16, 56, a5, a6, a7)
-		j GAME_LOOP	
+		j STANDBY	
 		# imprime tres vidas do player na aba do HUD
 		
 
 TWO_L:		la a0, life_2
 		render(a0, a1, a2, 16, 56, a5, a6, a7)	
-		j GAME_LOOP
+		j STANDBY
 		# imprime duas vidas do player na aba do HUD
 		
 ONE_L:		la a0, life
 		render(a0, a1, a2, 16, 56, a5, a6, a7)	
 		# imprime uma do player na aba do HUD		
 		
-		
-STANDBY:	li a7 32
+STANDBY:	li t0,0xFF200604		# carrega em t0 o endereco de troca de frame
+		sw a5,0(t0)			# mostra o sprite pronto para o usuario
+		li a7 32
 		li a0 2000
 		ecall				# freeza a tela inicial por 2 s antes do jogo comecar		
 		
 GAME_LOOP: 	la a0, POINTS
-		lh a0, 0(t0)			# carrega o n�mero de pontos em a0
-		li t0, 244			# numero total de pontos por fase
+		lh a0, 0(a0)			# carrega o número de pontos em a0
+		li t0, 246			# numero total de pontos por fase
 		beq a0, t0, NEXT_LEVEL		# caso o player alcance o fim do mapa, muda de fase
-		mv a0, s9
+		la a0, CURRENT
+		lh a0, 0(a0)
 		li a1, 268
 		li a2, 30
 		li a3, 0x00FF
@@ -137,14 +163,21 @@ GAME_LOOP: 	la a0, POINTS
 		li a7, 101 
 		ecall				# imprime os pontos do player
 		call KEY2
-		la t0, STAGE
+		la a0, PRESSED
+		lh t0, 0(a0)
+		beqz t0, NORMAL_LOOP		# caso o cheat n foi pressionado, continua o loop
+		li t0, 0
+		sh t0, 0(a0)			# reseta o PRESSED
+		j SETUP				# vai para o inicio da configuracao
+		
+NORMAL_LOOP:	la t0, STAGE
 		lh t0, 0(t0)			# carrega qual o nivel atual
 		la t1, CHAR_POS
 		lh t2, 2(t1)			# y do mario
 		lh t1, 0(t1)			# x do mario
 		beqz t0, STAGE_1
-		#########			# carrega o 2 mapa de colisao das moedas
-		#########			# verifica colisao de moedas no segundo mapa
+		la a0, moedas_2c		# carrega o 2 mapa de colisao das moedas
+		coin_check(a0, t1, t2)		# verifica colisao de moedas no segundo mapa
 		j STEP_2
 
 STAGE_1:	la a0, moedas_1c
@@ -154,7 +187,7 @@ STEP_2:		call MOVE
 		la a0, STAGE
 		lh a0, 0(a0)			# carrega qual mapa esta sendo renderizado
 		beqz a0, LEVEL_1
-		##########			# carrega o mapa 2
+		la a0, mapa_2			# carrega o mapa 2
 		j ERASE
 
 LEVEL_1:	la a0, mapa_1n
@@ -224,19 +257,7 @@ PRINT:		li a7, 0
 		ecall				# pausa por 50 ms
 		j GAME_LOOP
 
-NEXT_LEVEL:	li a7, 32
-		la a0, 1000			
-		ecall				# pausa por 1 s
-		la a0, black
-		li a1, 0
-		li a2, 0
-		li a5, 0
-		render_map(a0, a1, a2, 320, 240, a5)
-		li a5, 1
-		render_map(a0, a1, a2, 320, 240, a5)
-		# deixa a tela preta em ambos frames
-		mv t1, s9			# guarda a soma atual de pontos
-		la a0, STAGE
+NEXT_LEVEL:	la a0, STAGE
 		lh t2, 0(a0)
 		addi t2, t2, 1			# soma 1 ao nivel atual, logo, vai pra o proximo nivel
 		sh t2, 0(a0)
@@ -244,19 +265,22 @@ NEXT_LEVEL:	li a7, 32
 						
 GAME_OVER:	##########
 
-VICTORY:	
+VICTORY:	##########
 
 		
 .data
 
 	.include "Sprites_data/menu1.data"
+	.include "Sprites_data/black.data"
 	.include "Sprites_data/menu2.data"
-	.include "Sprites_data/mapa_1.data"
 	.include "Sprites_data/mapa_1n.data"
 	.include "Sprites_data/mapa_1c.data"
-	.include "Sprites_data/moedas_1.data"
 	.include "Sprites_data/moedas_1n.data"
 	.include "Sprites_data/moedas_1c.data"
+	.include "Sprites_data/mapa_2.data"
+	.include "Sprites_data/mapa_2c.data"
+	.include "Sprites_data/moedas_2.data"
+	.include "Sprites_data/moedas_2c.data"
 	.include "Sprites_data/mario_up.data"
 	.include "Sprites_data/mario_down.data"
 	.include "Sprites_data/mario_left.data"
